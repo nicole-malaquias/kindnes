5af082f7fd00ec72ec2987e3875127dde0276b8b
Emanuela Quizini <manu.biq@gmail.com>
2021-06-10 10:54:53 -0300

    feat: subscribe button specifi group

--- src/pages/SpecificGroup/index.js
+++ src/pages/SpecificGroup/index.js
@@ -1,12 +1,31 @@
 import { Redirect } from "react-router";
 // import Button from "../../components/Button";
+import { useEffect, useState } from "react";
 import { useAuthentication } from "../../Providers/authentication";
+import api from "../../services";
 
 const SpecificGroup = () => {
-  const { authenticated } = useAuthentication();
+  const { authenticated, token } = useAuthentication();
+  const [groupId, setGroupId] = useState("");
+
+  const handleSubscribe = (groupId) => {
+    api.post(`groups/${groupId}/subscribe/`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  };
+  useEffect(() => {
+    setGroupId(JSON.parse(localStorage.getItem("@Behabits:groupId")));
+  }, []);
+
   if (!authenticated) {
     return <Redirect to="/login" />;
   }
-  return <div>SpecificGroup</div>;
+  return (
+    <>
+      <button onClick={() => handleSubscribe(groupId)}>Subscribe</button>
+    </>
+  );
 };
 export default SpecificGroup;
