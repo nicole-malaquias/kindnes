b258b884fce71818101058b641bfd2c9412f0feb
Tanizia Fagundes <tdlf@icomp.ufam.edu.br>
2021-06-11 16:24:10 -0400

    feat: menu burg

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,7 +1,18 @@
-import { Container } from "./styles";
+import { Container, MenuBurg } from "./styles";
+import { Link } from "react-router-dom";
 import logo from "../../assets/logo.svg";
-const Menu = ({ isLogin }) => {
-  return !isLogin ? (
+import imgMenu from "../../assets/menu.png";
+import IconClosed from "../../assets/seta.png";
+import { useState } from "react";
+
+const Menu = ({ isLogin = false }) => {
+  const [isMenu, SetIsMenu] = useState(true);
+
+  const handleMenu = () => {
+    isMenu ? SetIsMenu(false) : SetIsMenu(true);
+  };
+
+  return isLogin ? (
     <Container>
       <div>
         <img src={logo} alt="logo"></img>
@@ -9,7 +20,29 @@ const Menu = ({ isLogin }) => {
       </div>
     </Container>
   ) : (
-    "renderir com menu burger e botão logaout"
+    <Container>
+      <div>
+        <button onClick={() => handleMenu()}>
+          <img src={imgMenu} alt="menu"></img>
+        </button>
+        <img src={logo} alt="logo"></img>
+        <h1>be habits</h1>
+      </div>
+      {isMenu ? (
+        <MenuBurg>
+          <button onClick={() => handleMenu()}>
+            <img src={IconClosed} alt="sair"></img>
+          </button>
+          <button onClick={() => handleMenu()}>
+            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/groups">Groups</Link>
+            <Link to="/specificgroup">Specific Group</Link>
+          </button>
+        </MenuBurg>
+      ) : (
+        ""
+      )}
+    </Container>
   );
 };
 export default Menu;
--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -3,16 +3,54 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-between;
   padding: 1rem;
+  position: relative;
 
   h1 {
     font-weight: normal;
+  }
+
+  button {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border: none;
+    background: none;
+    :focus {
+      outline: none;
+    }
+
+    a {
+      padding: 1rem;
+    }
+
+    img {
+    }
   }
 
   div {
     display: flex;
     align-items: center;
     margin: 1rem 3rem;
+  }
+`;
+
+export const MenuBurg = styled.div`
+  width: 60vh;
+  height: 100vh;
+  top: -1rem;
+  left: -3rem;
+  position: fixed;
+  z-index: 1;
+  background-color: var(--pink);
+  border-radius: 0px 20px 20px 0px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: end;
+
+  img {
+    padding: 2rem;
   }
 `;
