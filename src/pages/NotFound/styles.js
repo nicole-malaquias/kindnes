630bbaff2e9a3b47fa68cac2b86c0c1e9fd9880e
Julia Clepf <jumgc09@gmail.com>
2021-06-16 11:42:28 -0300

    style: fix footer position

--- src/pages/NotFound/styles.js
+++ src/pages/NotFound/styles.js
@@ -1,10 +1,18 @@
-import styled from "styled-components"; 
+import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
-  @media only screen and (max-width: 768px) {
-      flex-direction: column;
+  justify-content: space-between;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @media only screen and (min-width: 768px) {
+    flex-direction: row;
   }
 `;
