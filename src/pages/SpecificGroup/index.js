123be57a7d626a83fd7a87416795dd8ffa8f871f
Emanuela Quizini <manu.biq@gmail.com>
2021-06-16 09:17:20 -0300

    refactor: toastify

--- src/pages/SpecificGroup/index.js
+++ src/pages/SpecificGroup/index.js
@@ -1,7 +1,7 @@
 import GroupGoal from "../../components/GroupGoal";
 import GroupActivities from "../../components/GroupActivities";
 import GroupProgress from "../../components/GroupProgress";
-import api from "../../services";
+
 import { useAuthy } from "../../Providers/Authy";
 import { Redirect } from "react-router";
 
