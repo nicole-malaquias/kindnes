250902c37a27e5c03b3d82d4370210a6b1094fe3
Emanuela Quizini <manu.biq@gmail.com>
2021-06-15 22:27:02 -0300

    fix: getGroup fix

--- src/Providers/Group/index.js
+++ src/Providers/Group/index.js
@@ -1,4 +1,5 @@
 import { createContext, useContext, useState } from "react";
+import api from "../../services";
 
 const GroupContext = createContext();
 
@@ -6,14 +7,37 @@ export const GroupProvider = ({ children }) => {
   const [groupId, setGroupId] = useState(
     localStorage.getItem("@gestao:groupId") || ""
   );
-
+  const userId = localStorage.getItem("@gestao:user_Id") || "";
   const updateGroupId = (groupId) => {
     setGroupId(groupId);
     localStorage.setItem("@gestao:groupId", groupId);
   };
+  const [isSubscribe, setIsSubscribe] = useState(false);
+  const [group, setGroup] = useState({});
+  const getGroup = () => {
+    console.log(groupId);
+    api
+      .get(`groups/${groupId}/`)
+      .then((response) => {
+        console.log(response.data);
+        const { users_on_group } = response.data;
+        const users = users_on_group;
+        const nha = users.filter((elem) => elem.id === parseInt(userId));
+        console.log(nha);
+        setGroup(response.data);
+        if (nha.length > 0) {
+          setIsSubscribe(true);
+        } else {
+          setIsSubscribe(false);
+        }
+      })
+      .catch((e) => console.log(e));
+  };
 
   return (
-    <GroupContext.Provider value={{ groupId, updateGroupId }}>
+    <GroupContext.Provider
+      value={{ groupId, updateGroupId, getGroup, isSubscribe, group }}
+    >
       {children}
     </GroupContext.Provider>
   );
--- src/pages/SpecificGroup/index.js
+++ src/pages/SpecificGroup/index.js
@@ -3,14 +3,57 @@ import GroupActivities from "../../components/GroupActivities";
 import GroupProgress from "../../components/GroupProgress";
 import api from "../../services";
 import { useAuthy } from "../../Providers/Authy";
+import { useGroup } from "../../Providers/Group";
 import { Redirect } from "react-router";
+import { useEffect } from "react";
+import Button from "../../components/Button";
 
 const SpecificGroup = () => {
-  const { token, handleLogout } = useAuthy();
+  const { token, handleLogout, userId } = useAuthy();
+  const { groupId, getGroup, isSubscribe, group } = useGroup();
+
+  const handleSubscribe = () => {
+    api
+      .post(`groups/${groupId}/subscribe/`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        getGroup();
+        // toastSuccess("Yeah! You are now a member of this group");
+      });
+  };
+
+  const handleUnsubscribe = () => {
+    api
+      .delete(`/groups/${groupId}/unsubscribe/`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        getGroup();
+        // toastSuccess("Unsubscribe successfully");
+      })
+      .catch((e) => console.log(e));
+  };
+  useEffect(() => {
+    getGroup();
+  }, []);
 
   if (token) {
     return (
       <>
+        <h1>Blaaaaaaaaa</h1>
+        <h1>{group.name}</h1>
+        {isSubscribe ? (
+          <Button handleClick={handleUnsubscribe}>Unsubscribe</Button>
+        ) : (
+          <Button handleClick={handleSubscribe}>Subscribe</Button>
+        )}
         <GroupGoal />
         <GroupActivities />
         <GroupProgress />
